1c612cbd7604c8f4ce2ee8cb5560b6556d9b3d01
Manas Mulpuri <mulpuri.manas@gmail.com>
2025-03-04 22:58:22 -0800

    Parallelizing the evaluator

--- elo_evaluator/engine/base.py
+++ elo_evaluator/engine/base.py
@@ -5,3 +5,6 @@ class Engine(ABC):
     @abstractmethod
     def play(self, board: chess.Board) -> chess.Move:
         """Returns the best legal move from a given board."""
+
+    def __init__(self, name):
+        self._name = name
